8e02f4a13848d61ef231082a7554215ea05f894f
MarcusCCH <marcuscch12@gmail.com>
2023-04-24 02:43:36 +0800

    add 3d pig with waving hand animation

--- pig/pig.js
+++
@@ -1,5 +0,0 @@
-import { Application } from "@splinetool/runtime";
-console.log("connected to pig.js");
-const canvas = document.getElementById("canvas3d");
-const app = new Application(canvas);
-app.load("https://prod.spline.design/4spvtgEZ-WsZJQxM/scene.splinecode");
